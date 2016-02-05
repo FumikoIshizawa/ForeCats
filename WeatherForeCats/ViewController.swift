1fe1ef67c2f08fac6d059e45c0ae1dbfd7b378e8
FumikoIshizawa <p.krrr.731@gmail.com>
2016-02-05 11:48:00 +0900

    Initial Commit

---
+++ WeatherForeCats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForeCats
+//
+//  Created by FumikoIshizawa on 2016/02/05.
+//  Copyright © 2016年 fumikoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
